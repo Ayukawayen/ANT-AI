fcb51e4c9a50e3eaac2afc779a19440a13a40da5
Ayukawayen <Ayukawayen@Gmail.com>
2025-05-27 13:25:55 +0800

    add: dark mode

--- pages/v2/js/menu.js
+++ pages/v2/js/menu.js
@@ -1,4 +1,14 @@
+var setting = {
+	isDark:false,
+};
+
 window.addEventListener('load', onLoad_Menu);
+
+function onDarkToggleClick() {
+	setting.isDark = document.body.classList.toggle('dark');
+	
+	localStorage.setItem('___.setting', JSON.stringify(setting));
+}
 
 function onMenuIconClick(ev) {
 	let node = document.querySelector('header >menu');
@@ -12,6 +22,14 @@ function onMenuItemClick(aid) {
 }
 
 function onLoad_Menu() {
+	let data = localStorage.getItem('___.setting')||'{}';
+	data = JSON.parse(data);
+	
+	setting = data;
+
+	document.querySelector('body').classList.toggle('dark', setting.isDark);
+	document.querySelector('#loading').classList.remove('show');
+	
 	document.body.addEventListener('click', ()=>{
 		document.querySelector('header >menu').setAttribute('isShow', 'false');
 	});
