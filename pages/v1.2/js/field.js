a30039efa3cc06f613c1ac3c31e23dc533d118cb
Ayukawayen <Ayukawayen@Gmail.com>
2025-05-30 12:45:59 +0800

    add: v1.2

---
+++ pages/v1.2/js/field.js
@@ -0,0 +1,62 @@
+var fields = {
+	'user':'',
+	'user.description':'',
+	'char':'',
+	'char.description':'',
+	'char.scenario':'',
+	'char.example':'',
+	'char.memory':'',
+	'context_size':'',
+	'prompt':'',
+	'parameter':'',
+};
+
+window.addEventListener('load', onLoad_Field);
+
+function onResetClick() {
+	refreshFieldNodes();
+}
+
+function onSaveClick() {
+	for(let k in fields) {
+		let node = document.querySelector(`[field-key="${k}"]`);
+		if(!node) continue;
+		
+		fields[k] = node.value;
+	}
+	
+	onFieldChange();
+}
+
+function onFieldChange() {
+	localStorage.setItem(chatId + '.field', JSON.stringify(fields));
+	onChatChange();
+}
+
+function onLoad_Field() {
+	let data = localStorage.getItem(chatId + '.field');
+	if(!data) return;
+	
+	data = JSON.parse(data);
+	
+	for(let k in fields) {
+		fields[k] = data[k]||fields[k];
+	}
+	
+	refreshFieldNodes();
+}
+
+function refreshFieldNodes() {
+	for(let k in fields) {
+		let node = document.querySelector(`[field-key="${k}"]`);
+		if(!node) continue;
+		
+		node.value = fields[k];
+	}
+}
+
+function getFieldValue(fieldKey) {
+	let node = document.querySelector(`[field-key="${fieldKey}"]`);
+	
+	return node.value;
+}
